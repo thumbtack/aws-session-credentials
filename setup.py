e06931cb0b60a9afa0cbd20d5a3ac606c8b1cd51
Glen Oakley <glen@thumbtack.com>
2018-03-21 14:53:46 -0700

    Unset AWS_PROFILE before running the script

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(README, 'r') as handle:
 
 setuptools.setup(
     name='aws-session-credentials',
-    version='0.1.2',
+    version='0.1.3',
     description='Manage AWS session credentials',
     long_description=LONG_DESCRIPTION,
     url='https://github.com/thumbtack/aws-session-credentials',
